64e6e4f3307e75c7f53aff64bc82d30cdd0b6732
albertjou <albert_jou@hotmail.com>
2013-11-21 10:26:49 +1100

    Signin modal created

--- app/assets/javascripts/guitarbuild.js
+++ app/assets/javascripts/guitarbuild.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $('#continue').click(function(event) {
+    event.preventDefault();
     alert("Button was clicked");
-    preventDefault();
-    $('.meter').css('width', progressValue);
+    $('.meter').css('width', 40%);
   });
 
 
@@ -10,4 +10,27 @@ $(document).ready(function() {
 
 
 // Method to continue onto the next section for instrument
+var create_guitar = function() {
+  var step = $('.step').text();
+  var body_color = $('#guitar-body-color').val();
+  var nut_material = $('#nut-material').val();
+  var nut_width = $('#nut-width').val();
+
+$.ajax({
+  url: '/guitars/new',
+  type: 'get',
+  dataType: 'json',
+  data: {id: id, body_color: body_color, nut_material: nut_material, nut_width: nut_width}
+})
+.done(function() {
+  console.log("success");
+})
+.fail(function() {
+  console.log("error");
+})
+.always(function() {
+  console.log("complete");
+});
+
+};
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Customacoustic::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  devise_for :users
+  devise_for :users, :controllers => {sessions: 'sessions'}
   ActiveAdmin.routes(self)
 
 
