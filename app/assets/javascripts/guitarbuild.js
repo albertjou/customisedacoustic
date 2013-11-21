2e9a12c0f9a3dcf2bb3d4957ef03b9d497405a8b
albertjou <albert_jou@hotmail.com>
2013-11-21 15:23:38 +1100

    jQuery for customisations table complete

--- app/assets/javascripts/guitarbuild.js
+++ app/assets/javascripts/guitarbuild.js
@@ -1,12 +1,56 @@
 $(document).ready(function() {
   $('#continue').click(function(event) {
+    var $current = $('.current');
     event.preventDefault();
-    alert("Button was clicked");
-    $('.meter').css('width', 40%);
+    step_forward($current);
+    update_table($current);
   });
 
+  $('#save').click(function(event) {
+    // First check if session
+    // Then add to database
+    // Add status
+  });
 
 });
+
+// Hide the current shop and move onto the next one
+var step_forward = function($current) {
+  var nextOne = $current.next();
+  $current.addClass('invisible complete');
+  nextOne.addClass('current');
+  nextOne.removeClass('invisible incomplete')
+  $current.removeClass('current');
+  $('.orbit-container').foundation('reflow');
+};
+
+// Add selection to table upon click
+var update_table = function($current) {
+  // Check which one is active
+  if ($current.hasClass('guitar-shop')) {
+    var color = $('#guitar-body-color').val();
+    populate_table("Body color: " + color, "N/A").prependTo('tbody');
+    var nut_material = $('#nut-material').val();
+    populate_table("Nut material: " + nut_material, "N/A").prependTo('tbody');
+    var nut_width = $('#nut-width').val();
+    populate_table("Nut width: " + nut_width, "N/A").prependTo('tbody');
+  } else {
+    var item = $current.find('.active .product-name').data('name');
+    var price = parseInt($current.find('.active .product-price').data('price'));
+    populate_table(item, price).prependTo('tbody');
+  }
+
+
+};
+
+// Creates the table html
+var populate_table = function(item, price) {
+  var $item = $('<td>').text(item);
+  var $price = $('<td>').text(price);
+  var newRow = $('<tr>');
+  newRow = newRow.append($item, $price);
+  return newRow;
+};
 
 
 // Method to continue onto the next section for instrument
@@ -15,12 +59,16 @@ var create_guitar = function() {
   var body_color = $('#guitar-body-color').val();
   var nut_material = $('#nut-material').val();
   var nut_width = $('#nut-width').val();
+  var body_shape = parseInt($('.active .body_id').text().replace(/"/g, ''));
+  var fretboard = parseInt($('.active .fretboard_id').text().replace(/"/g, ''));
+  var bridge = parseInt($('.active .bridge_id').text().replace(/"/g, ''));
+  var guitar_string = parseInt($('.active .guitar_string_id').text().replace(/"/g, ''));
 
 $.ajax({
   url: '/guitars/new',
   type: 'get',
   dataType: 'json',
-  data: {id: id, body_color: body_color, nut_material: nut_material, nut_width: nut_width}
+  data: {id: id, body_color: body_color, nut_material: nut_material, nut_width: nut_width, body_shape: body_shape, fretboard: fretboard, bridge: bridge, guitar_string: guitar_string}
 })
 .done(function() {
   console.log("success");
